1a46d318573311d1976211939bfafabd823775ef
Thomas Rayner <thmsrynr@outlook.com>
2020-04-02 11:25:52 -0700

    add day 1 and 2

---
+++ day01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an array to check: ");
+            string intString = Console.ReadLine();
+            int[] ints = intString.Split(',').Select(n => int.Parse(n)).ToArray();
+
+            var sol = new Solution();
+            int final = sol.SingleNumber(ints);
+            Console.WriteLine(final.ToString());
+        }
+    }
+
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            return nums.GroupBy(x => x).Where(y => y.Count() == 1).Select(z => z.Key).FirstOrDefault();
+        }
+    }
+}
---
+++ day02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var happinessChecker = new HappinessChecker();
+
+            Console.Write("Check number: ");
+            int check = Convert.ToInt32(Console.ReadLine());
+            var isHappy = happinessChecker.IsHappy(check);
+
+            if (isHappy)
+            {
+                Console.WriteLine("true");
+            }
+
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+
+    class HappinessChecker{
+        public bool IsHappy(int n)
+        {
+            HashSet<int> seenNumbers = new HashSet<int>();
+            int route = NumberCheck(n);
+
+            while (seenNumbers.Add(route))
+            {
+                if (route == 1)
+                {
+                    return true;
+                }
+
+                route = NumberCheck(route);
+            }
+
+            return false;
+        }
+
+        public int NumberCheck(int num)
+        {
+            int calc = 0;
+            List<int> listOfInts = new List<int>();
+
+            while (num > 0)
+            {
+                listOfInts.Add(num % 10);
+                num = num / 10;
+            }
+
+            foreach (int digit in listOfInts)
+            {
+                calc += Convert.ToInt32(Math.Pow(digit, 2));
+            }
+
+            return calc;
+        }
+    }
+}
