abb056ca9732608ca06751dfcf3403064e7c7e66
Thomas Rayner <thmsrynr@outlook.com>
2020-04-04 20:04:02 -0700

    day04

---
+++ day04/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an array to check: ");
+            string intString = Console.ReadLine();
+            int[] ints = intString.Split(',').Select(n => int.Parse(n)).ToArray();
+
+            var sol = new Solution();
+            sol.MoveZeroes(ints);
+            Console.WriteLine(string.Join(", ", ints));
+        }
+    }
+
+    public class Solution
+    {
+        // Input: [0,1,0,3,12]
+        // Output: [1,3,12,0,0]
+        public void MoveZeroes(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                   nums[count] = nums[i];
+                   count++;
+                }
+            }
+
+            while (count < nums.Length)
+            {
+                nums[count] = 0;
+                count++;
+            }
+        }
+    }
+}
