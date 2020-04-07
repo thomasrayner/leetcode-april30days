b35ead0e9325ecd564407f8457b3b604af7c2c04
Thomas Rayner <thmsrynr@outlook.com>
2020-04-07 11:32:02 -0700

    day 07

---
+++ day07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an array to check: ");
+            string intString = Console.ReadLine();
+            int[] ints = intString.Split(',').Select(n => int.Parse(n)).ToArray();
+
+            var sol = new Solution();
+            int final = sol.CountElements(ints);
+            Console.WriteLine(final.ToString());
+        }
+    }
+
+    public class Solution
+    {
+        public int CountElements(int[] arr)
+        {
+            int finalCount = 0;
+
+            foreach (int check in arr.Distinct())
+            {
+                if (arr.Contains(check + 1))
+                {
+                    finalCount += arr.Where(x => x == check).Count();
+                }
+            }
+
+            return finalCount;
+        }
+    }
+}
